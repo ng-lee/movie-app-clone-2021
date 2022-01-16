4080f616e942b7baded3505792de053639552dc1
codeer <loginonlyyy@gmail.com>
2022-01-16 22:08:21 +0900

    usage of useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL AN API...");
+  }, []);
   return (
     <div>
-      <h1>Welcome back to React</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
